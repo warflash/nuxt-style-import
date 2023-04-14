8f4d4f4082cfcbece7aaf534858a6027ed42e3ba
warflash <nwiesinger67@gmail.com>
2023-04-14 16:33:51 +0200

    testing

---
+++ components/child.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>I'm a styled text</div>
+</template>
+
+<style scoped>
+div {
+  font-size: 200px;
+  color: red;
+}
+</style>
--- components/clientParent.client.vue
+++ components/clientParent.client.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
-    <ClientParent />
+    Parent Text
+    <Child />
   </div>
 </template>
