7682cbfb13ba9602e9525ce66b3eb63e084890c1
warflash <nwiesinger67@gmail.com>
2023-04-14 16:47:27 +0200

    test

--- pages/index.vue
+++ pages/index.vue
@@ -2,5 +2,6 @@
   <div>
     Hello
     <NuxtLink to="/broken">To Broken </NuxtLink>
+    <NuxtLink to="/test">To Test </NuxtLink>
   </div>
 </template>
---
+++ pages/test.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="prevent-full-width mx-auto flex">
+    <client-only>
+      <div class="sidebar-container bg-base-500 mr-1 rounded p-2">Hello</div>
+      <Child class="settings-container" />
+    </client-only>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.sidebar-container {
+  width: 15rem;
+  position: sticky;
+  top: 58px;
+  align-self: flex-start;
+}
+.settings-container {
+  width: calc(100% - 15rem);
+}
+</style>
