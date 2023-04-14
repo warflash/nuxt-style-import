184a344615532deed15d642ecff005465c0694b9
warflash <nwiesinger67@gmail.com>
2023-04-14 16:49:18 +0200

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  vite: {
+    build: {
+      sourcemap: false,
+      modulePreload: {
+        resolveDependencies: () => [],
+      },
+    },
+  },
+});
