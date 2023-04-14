299cc9542eaa6fdc57b82b3031ede4dcb21481c0
warflash <nwiesinger67@gmail.com>
2023-04-14 03:16:40 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nuxt-style-import
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-2vlmkn)
---
+++ app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>Hello!</div>
+</template>
+
+<script>
+import './assets/test.css';
+</script>
